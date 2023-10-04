7f4696d1a6de83a00fc1adf9f7cc6141f200fd1b
Руслан Жевраков <116262017+RyslanZevrakov@users.noreply.github.com>
2023-10-04 19:34:28 +0300

    Add files via upload

---
+++ MMORPG-main/MMORPG-main/MMORPG/src/main/java/org/example/Skills.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public interface Skills {
+    void addSkills(ArrayList<String> skills);
+
+
+    void sirstSkill();
+
+    void secondSkill();
+
+    void thirdSkill();
+}
