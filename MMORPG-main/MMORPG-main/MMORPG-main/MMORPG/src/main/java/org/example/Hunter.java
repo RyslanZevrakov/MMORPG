3d29820d7283762c58a7c6a9d9f32d4bf3cda384
Руслан Жевраков <116262017+RyslanZevrakov@users.noreply.github.com>
2023-10-04 19:25:42 +0300

    Add files via upload

---
+++ MMORPG-main/MMORPG-main/MMORPG-main/MMORPG/src/main/java/org/example/Hunter.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Hunter implements Skills {
+    private ArrayList<String> skills = new ArrayList<>();
+
+    @Override
+    public void addSkills(ArrayList<String> skills) {
+        skills.add("Соколиное зрение");
+        skills.add("Выстрел из лука");
+        skills.add("Поиск крови");
+    }
+
+    int numder = (int) (Math.random() * 4) + 2;
+
+    public void hunter() throws java.io.IOException {
+        addSkills(skills);
+        System.out.println("Информация");
+        System.out.println(" 1. Соколиное зрение");
+        System.out.println(" 2. Выстрел из лука");
+        System.out.println(" 3. Поиск крови");
+        System.out.print("Bыберете вариант: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int choice = scanner.nextInt();
+
+        System.out.println();
+
+        switch (choice) {
+            case 1:
+                sirstSkill();
+                break;
+            case 2:
+                secondSkill();
+                break;
+            case 3:
+                thirdSkill();
+                break;
+
+        }
+    }
+
+
+    @Override
+    public void sirstSkill() {
+
+        if (numder == 3) {
+            System.out.println("Спасобность " + skills.get(0) + " сработала");
+        } else {
+            System.out.println("Спасобность " + skills.get(0) + " не сработала");
+        }
+    }
+
+    @Override
+    public void secondSkill() {
+        if (numder == 4) {
+            System.out.println("Спасобность " + skills.get(1) + " сработала");
+        } else {
+            System.out.println("Спасобность " + skills.get(1) + " не сработала");
+        }
+    }
+
+    @Override
+    public void thirdSkill() {
+        if (numder == 5) {
+            System.out.println("Спасобность " + skills.get(2) + " сработала");
+        } else {
+            System.out.println("Спасобность " + skills.get(2) + " не сработала");
+        }
+    }
+}
